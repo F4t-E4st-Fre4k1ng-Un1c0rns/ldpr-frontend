428f063093ebfd7cd4410eea902696cb2d86855d
turleo <turleo@users.noreply.github.com>
2025-04-19 09:19:07 +0000

    deploy: a0e648656fe970a26cc0e50fc81f3babba17ed3e

---
+++ assets/Header.stories-t1uezmxC.js
@@ -0,0 +1,15 @@
+import{u as e}from"./hooks.module-CAf6ubPb.js";import{E as s}from"./preact-router-D-9jkRxq.js";import"./preact.module-rHvS-6NF.js";const i=()=>e("header",{className:"bg-base-200",children:e("div",{className:"container mx-auto navbar",children:[e("div",{className:"flex-1",children:e(s,{className:"btn btn-ghost text-xl",href:"/",children:"ЛДПР.Аниме"})}),e("nav",{className:"flex-none",children:e("ul",{className:"menu menu-horizontal px-1",children:[e("li",{children:e(s,{className:"btn btn-ghost",href:"/",children:"Home"})}),e("li",{children:e(s,{className:"btn btn-ghost",href:"/anime",children:"Anime List"})})]})})]})}),h={component:i,parameters:{layout:"fullscreen"},title:"Components/Header"},r={args:{},parameters:{env:{VITE_PROJECT_NAME:"LDPR"}}},a={args:{},parameters:{env:{VITE_PROJECT_NAME:"My Custom Project"}}};var n,t,o;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    env: {
+      VITE_PROJECT_NAME: "LDPR"
+    }
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var c,m,l;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    env: {
+      VITE_PROJECT_NAME: "My Custom Project"
+    }
+  }
+}`,...(l=(m=a.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const E=["Default","CustomProjectName"];export{a as CustomProjectName,r as Default,E as __namedExportsOrder,h as default};
