a66fb42efc9ab2c1419bfb2086c9b3b6243ba0bc
turleo <turleo@users.noreply.github.com>
2025-04-19 07:36:02 +0000

    deploy: d62561afd50c503814c0b0ea5fec2cf6b0ac0e3c

---
+++ assets/Header.stories-DW4aVetZ.js
@@ -0,0 +1,15 @@
+import{u as a}from"./jsxRuntime.module-BxqgnTr_.js";import"./preact.module-rHvS-6NF.js";const p=()=>a("div",{className:"navbar bg-base-200",children:a("div",{className:"flex-1",children:a("a",{className:"btn btn-ghost text-xl",children:"LDPR"})})}),E={component:p,parameters:{layout:"fullscreen"},title:"Components/Header"},e={args:{},parameters:{env:{VITE_PROJECT_NAME:"LDPR"}}},r={args:{},parameters:{env:{VITE_PROJECT_NAME:"My Custom Project"}}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    env: {
+      VITE_PROJECT_NAME: "LDPR"
+    }
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    env: {
+      VITE_PROJECT_NAME: "My Custom Project"
+    }
+  }
+}`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const i=["Default","CustomProjectName"];export{r as CustomProjectName,e as Default,i as __namedExportsOrder,E as default};
---
+++ assets/preview-D14K2O-p.js
@@ -0,0 +1,1 @@
+const e={parameters:{controls:{matchers:{color:/(background|color)$/iu,date:/Date$/iu}}}};export{e as default};
