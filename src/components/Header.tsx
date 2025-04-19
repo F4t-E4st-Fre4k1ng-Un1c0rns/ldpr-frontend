a0e648656fe970a26cc0e50fc81f3babba17ed3e
Valya Tureyskaya <me@turleo.me>
2025-04-19 19:18:15 +1000

    🔧: Refactor Header component for improved structure and accessibility
    - Change the outer div to a header element for semantic HTML.
    - Update navigation links to include proper class names and structure.
    - Maintain type safety with comments for preact-router types.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,32 @@ import { Link } from "preact-router";
 
 const Header = () => {
   return (
-    <div className="navbar bg-base-200">
-      <div className="flex-1">
-        <Link href="/" className="btn btn-ghost text-xl">ЛДПР.Аниме</Link>
+    <header className="bg-base-200">
+      <div className="container mx-auto navbar">
+        <div className="flex-1">
+          {/* @ts-expect-error seems like error in preact-router types */}
+          <Link className="btn btn-ghost text-xl" href="/">
+            ЛДПР.Аниме
+          </Link>
+        </div>
+        <nav className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              {/* @ts-expect-error seems like error in preact-router types */}
+              <Link className="btn btn-ghost" href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              {/* @ts-expect-error seems like error in preact-router types */}
+              <Link className="btn btn-ghost" href="/anime">
+                Anime List
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li><Link href="/" className="btn btn-ghost">Home</Link></li>
-          <li><Link href="/anime" className="btn btn-ghost">Anime List</Link></li>
-        </ul>
-      </div>
-    </div>
+    </header>
   );
 };
 
